c665cfa815b1cbe815cf7241fc5bfd05107fd397
root <root@DESKTOP-1E5OKAG>
2023-01-18 20:26:32 +0200

    add algorithms

---
+++ linked_list.py
@@ -0,0 +1,184 @@
+"""
+question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
+if question in ["42", "forty two", "forty-two"] :
+    print("Yes")
+else:
+    print("No")
+    """
+"""
+class Cookie:
+    def __init__(self, colour):
+        self.colour = colour
+
+    def get_colour(self):
+        return self.colour
+
+    def set_colour(self, colour):
+        self.colour = colour
+
+cookie1 = Cookie("green")
+cookie2 = Cookie("blue")
+c = cookie1.get_colour()
+col = cookie2.get_colour()
+print(c, col)
+
+
+cookie1.set_colour("pink")
+
+print(cookie1.get_colour(), cookie2.get_colour())
+
+"""
+"""
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+new_linked_list = LinkedList(4)
+print(new_linked_list.head.value)
+"""
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+        return True
+
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+
+        return temp
+
+    def get(self, index):
+        if 0 > index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if 0 > index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index-1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        return temp
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+        after = temp.next
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+
+
+
+
+my_linked_list = LinkedList(11)
+my_linked_list.append(23)
+my_linked_list.append(3)
+my_linked_list.append(7)
+my_linked_list.set_value(2, 2)
+my_linked_list.print_list()
