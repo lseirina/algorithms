fb2f560af93f627601d20c85c3e875d6d1858216
root <lseirina87@gmail.com>
2023-01-28 21:44:49 +0200

    add BFS

--- bst.py
+++ bst.py
@@ -78,9 +78,9 @@ class BinarySearchTree:
     def __delet_node(self, current_node, value):
         if current_node == None:
             return None
-        if current_node.value > value:
+        if value < current_node.value:
             current_node.left = self.__delet_node(current_node.left, value)
-        if current_node.value < value:
+        if value > current_node.value:
             current_node.right = self.__delet_node(current_node.right, value)
         else:
             if current_node.left == None and current_node.right == None:
@@ -99,17 +99,34 @@ class BinarySearchTree:
         self.__delet_node(self.root, value)      
     
     
-    
-    
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None: 
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
 
 my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
 
+print(my_tree.BFS())
 
-my_tree.delet_node(2)
+# my_tree.delet_node(2)
 
-print(my_tree.min_value(my_tree.root.value))
-print(my_tree.min_value(my_tree.root.left.value))
-print(my_tree.min_value(my_tree.root.right.value))
+# print(my_tree.min_value(my_tree.root).value)
+# print(my_tree.min_value(my_tree.root.left).value)
+# print(my_tree.min_value(my_tree.root.right).value)
+
